dbed8eeba5c61f496f2a0824162f2f93eeb89385
Zolo101 <umadeguy@gmail.com>
2020-03-20 08:22:00 +0000

    5beam qt 1.0

--- 5beam/MainWindow.xaml.cs
+++ 5beam/MainWindow.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,7 +18,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using WK.Libraries.BetterFolderBrowserNS;
+//using WK.Libraries.BetterFolderBrowserNS;
 using Path = System.IO.Path;
 
 namespace _5beam {
@@ -22,8 +26,20 @@ namespace _5beam {
 	/// version 1.0
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
+	/// 
 	/// </summary>
+	/// 
+
+	public class Level {
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Username { get; set; }
+		public string Description { get; set; }
+		public int Size { get; set; }
+		public int Views { get; set; }
+	}
 	public partial class MainWindow : Window {
+		const string database = "http://5beam.zapto.org/api/levellist";
 		static string directory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
 		string fivebPath = Path.Combine(directory, "5b.swf");
 		string levelsPath = Path.Combine(directory, "levels.txt");
@@ -34,9 +50,43 @@ namespace _5beam {
 		Boolean fiveb = true;
 
 		public void Refresh() {
-			foreach (var file in Directory.GetFiles(levelfolderPath, "*.txt")) {
-				Levelslist.Items.Add(Path.GetFileName(file));
+			Levelslist.Items.Clear();
+
+			var levelRequest = WebRequest.Create(database);
+
+			var levelStream = levelRequest.GetResponse().GetResponseStream();
+
+			if (levelStream != null) {
+				using (var streamReader = new StreamReader(levelStream)) {
+					// Return next avaliable character or -1 if there are no
+					// characters to be read.
+					while (streamReader.Peek() > -1) {
+						ParseStream(streamReader.ReadLine());
+					}
+				}
+			} else {
+				Levelslist.Items.Add("Refresh Failed. Check if you & the server is online.");
 			}
+			//Levelslist.Items.Add(levelStream);
+			//Console.WriteLine(levelStream);
+
+			//foreach (var file in Directory.GetFiles(levelfolderPath, "*.txt")) {
+			//	Levelslist.Items.Add(Path.GetFileName(file));
+			//}
+		}
+
+		public void ParseStream(string jsonlevellist) {
+			JavaScriptSerializer js = new JavaScriptSerializer();
+			Level[] levellist = js.Deserialize<Level[]>(jsonlevellist);
+
+			for (int i = 0; i < levellist.Length; i++) {
+				Levelslist.Items.Add(
+					levellist[i].Name + " (Uploaded By: " +
+					levellist[i].Username + ", Views: " +
+					levellist[i].Views + ")"
+				);
+			}
+
 		}
 
 		public MainWindow() {
@@ -45,9 +95,9 @@ namespace _5beam {
 				fiveb = false;
 				MessageBox.Show("5beam cannot find the 5b swf. It should be in '" + fivebPath + "' You can get one at http://battlefordreamisland.com/5b/5b.swf");
 			}
-			if (!(File.Exists(levelsPath))) {
-			    MessageBox.Show("levels.txt doesn't seem to exist! In order for the 5b swf to function, you'll need to SELECT a txt file from the listbox.");
-			}
+			//if (!(File.Exists(levelsPath))) {
+			//    MessageBox.Show("levels.txt doesn't seem to exist! In order for the 5b swf to function, you'll need to SELECT a txt file from the listbox.");
+			//}
 			if (!(File.Exists(configPath))) {
 				File.WriteAllText(configPath, levelfolderPath);
 			} else {
@@ -59,19 +109,34 @@ namespace _5beam {
 		}
 
 		private void Start5b_Click(object sender, RoutedEventArgs e) {
-			if (selectedlevel != null) {
-				string levelbuffer = File.ReadAllText(Path.Combine(levelfolderPath, selectedlevel));
-				File.WriteAllText(levelsPath,levelbuffer);
+			if (fiveb) {
+				//System.Diagnostics.Process.Start(fivebPath);
+			}
+			else {
+				MessageBox.Show("The 5b.swf file needs to be in '" + directory + "' for this to work. If it is, restart this program.");
+				return;
 			}
 
-			if (fiveb) {
-				System.Diagnostics.Process.Start(fivebPath);
-			} else {
-				MessageBox.Show("The 5b.swf file needs to be in '" + directory + "' for this to work. If it is, restart this program.");
+			if (selectedlevel != null) {
+				Thread downloadThread = new Thread(() => {
+					WebClient client = new WebClient();
+					client.DownloadFileCompleted += new System.ComponentModel.AsyncCompletedEventHandler(client_DownloadFileCompleted);
+					client.DownloadFileAsync(new Uri("http://5beam.zapto.org/dl/0"), levelsPath);
+				});
+				downloadThread.Start();
+				//string levelbuffer = File.ReadAllText(Path.Combine(levelfolderPath, selectedlevel));
+				//File.WriteAllText(levelsPath,levelbuffer);
 			}
+
 		}
 
-		private void Looklevels_Click(object sender, RoutedEventArgs e) {
+		void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e) {
+			Dispatcher.BeginInvoke((Action) delegate {
+				System.Diagnostics.Process.Start(fivebPath);
+			});
+		}
+
+		/*private void Looklevels_Click(object sender, RoutedEventArgs e) {
 			var betterFolderBrowser = new BetterFolderBrowser {
 				Title = "Please select a folder.",
 				RootFolder = directory
@@ -84,7 +149,7 @@ namespace _5beam {
 				Levelslist.Items.Clear();
 				Refresh();
 			}
-		}
+		}*/
 
 		private void Levelslist_SelectionChanged(object sender, SelectionChangedEventArgs e) {
 			selectedlevel = Levelslist.SelectedItem.ToString();
@@ -93,6 +158,15 @@ namespace _5beam {
 		private void RefreshButton_Click(object sender, RoutedEventArgs e) {
 			Levelslist.Items.Clear();
 			Refresh();
+		}
+
+		private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+
+		}
+
+		private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+
 		}
 	}
 }

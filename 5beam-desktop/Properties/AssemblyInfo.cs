30cc94f1242205746dd0ec49d31898cfbdb282e7
arimeisels3@gmail.com <arimeisels3@gmail.com>
2020-11-08 14:26:47 -0500

    Now saves progress on individual levelpacks.

--- 5beam-desktop/Properties/AssemblyInfo.cs
+++ 5beam-desktop/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("5beam")]
-[assembly: AssemblyDescription("The easier way to play custom made levels.")]
+[assembly: AssemblyTitle("5beam-desktop")]
+[assembly: AssemblyDescription("The easiest way to play custom made levels for 5b.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Zelo101")]
-[assembly: AssemblyProduct("5beam")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("5beam-desktop")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
--- README.md
+++ README.md
@@ -4,14 +4,9 @@ Desktop application that allows you to play 5b levels from the 5beam database to
 ## Building
 I've used Visual Studio 2019 to build this.
 
-## Legacy Building
-I used Visual Studio 2017 to build this. The dependencies are [BetterFolderBrowser](https://www.nuget.org/packages/BetterFolderBrowser/).
-
 ## Notes
 **It is greatly recommended that you put 5beam in a folder before starting for the first time.**
 
 **For any bugs, please start an issue in the issue tab (or contact me on discord)**. 
 
 If you want to play the last played level file, just press "Play 5b".
-
-The config.zelo file is where 5beam stores the level directory.
